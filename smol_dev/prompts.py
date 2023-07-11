63aa8eeac0827ff5475f069c84ff6b6cef1af3dc
afik.cohen <aphexsa@gmail.com>
2023-07-10 17:41:39 -0700

    Remove stray comments

--- smol_dev/prompts.py
+++ smol_dev/prompts.py
@@ -9,21 +9,18 @@ from tenacity import (
     retry,
     stop_after_attempt,
     wait_random_exponential,
-)  # for exponential backoff
+)
 
 openaimodel = "gpt-3.5-turbo-0613"
-# openaimodel = "gpt-4-0613"
 
-smol_dev_system_prompt = """
-You are a top tier AI developer who is trying to write a program that will generate code for the user based on their intent.
-Do not leave any todos, fully implement every feature requested.
+smol_dev_system_prompt = """                                                                                                                                                                                                                          
+You are a top tier AI developer who is trying to write a program that will generate code for the user based on their intent.                                                                                                                          
+Do not leave any todos, fully implement every feature requested.                                                                                                                                                                                      
 """
 
 
 @openai_function
 def file_paths(files_to_edit: List[str]) -> List[str]:
-    """Takes a list of filepaths relative to the current directory and returns a list of filepaths that need to be
-    edited."""
     print("filesToEdit", files_to_edit)
     return files_to_edit
 
@@ -37,12 +34,12 @@ def specify_file_paths(prompt: str, plan: str) -> List[str]:
         messages=[
             {
                 "role": "system",
-                "content": f"""{smol_dev_system_prompt}
-          
-      When given their intent, create a complete, exhaustive list of filepaths that the user would write to make the program.
-      
-      only list the filepaths you would write, and return them as a python list of strings. 
-      do not add any other explanation, only return a python list of strings.
+                "content": f"""{smol_dev_system_prompt}                                                                                                                                                                                               
+
+      When given their intent, create a complete, exhaustive list of filepaths that the user would write to make the program.                                                                                                                         
+
+      only list the filepaths you would write, and return them as a python list of strings.                                                                                                                                                           
+      do not add any other explanation, only return a python list of strings.                                                                                                                                                                         
                   """,
             },
             {
@@ -59,26 +56,21 @@ def specify_file_paths(prompt: str, plan: str) -> List[str]:
     return result
 
 
-# def plan(prompt: str, filePaths: List[str]):
 def plan(prompt: str, stream_handler: Optional[Callable[[bytes], None]] = None):
     completion = openai.ChatCompletion.create(
         model=openaimodel,
         temperature=0.7,
-        stream=True,  # always stream
+        stream=True,
         messages=[
             {
                 "role": "system",
-                "content": f"""{smol_dev_system_prompt}
-      
-  In response to the user's prompt, 
-  Please name and briefly describe the structure of the app we will generate, including, for each file we are generating, what variables they export, data schemas, id names of every DOM elements that javascript functions will use, message names, and function names.
+                "content": f"""{smol_dev_system_prompt}                                                                                                                                                                                               
+
+  In response to the user's prompt,                                                                                                                                                                                                                   
+  Please name and briefly describe the structure of the app we will generate, including, for each file we are generating, what variables they export, data schemas, id names of every DOM elements that javascript functions will use, message names, 
+and function names.                                                                                                                                                                                                                                   
                   """,
-                # Exclusively focus on the names of the shared dependencies, and do not add any other explanation.
             },
-            # {
-            #     "role": "user",
-            #     "content": f""" the files we have decided to generate are: {filePaths} """,
-            # },
             {
                 "role": "user",
                 "content": f""" the app prompt is: {prompt} """,
@@ -88,8 +80,7 @@ def plan(prompt: str, stream_handler: Optional[Callable[[bytes], None]] = None):
 
     collected_messages = []
     for chunk in completion:
-        # chunk_message = chunk['choices'][0]['delta']  # extract the message
-        chunk_message = chunk["choices"][0]["delta"]
+        chunk_message = chunk["choices"][0]["delta"]  # extract the message
         collected_messages.append(chunk_message)  # save the message
         if stream_handler:
             try:
@@ -101,34 +92,6 @@ def plan(prompt: str, stream_handler: Optional[Callable[[bytes], None]] = None):
     return full_reply_content
 
 
-#####
-# TODO: explore pydantic models for export to morph/rift
-#####
-# from pydantic import BaseModel, Field # probably doesn't compile
-
-# class CodeFile(BaseModel):
-#     file_name: str = Field("Name of the file")
-#     file_contents: str = Field("Contents of the file. Must compile correctly")
-
-# class SmolDeveloperManifest(BaseModel):
-#     md_contents = Field("flat md file which specifies the directory tree of the app we are generating")
-
-# # we want to compute this result incrementally through a chain of thought, i.e.
-# # first, synthesize the manifest
-# # then synthesize partial codefiles with filenames and filepaths
-# # then for all codefiles synthesize content
-# class SmolDeveloperResult(BaseModel):
-#     manifest: SmolDeveloperManifest
-#     code_files: List[CodeFile]
-
-# # commented out because of https://github.com/jxnl/openai_function_call/issues/32 # its because need to escape json
-# https://community.snowflake.com/s/article/Escaping-new-line-character-in-JSON-to-avoid-data-loading-errors
-# @openai_function def validCodeFile(codeFile: str) -> str: """Receives a string of valid code and, after checking
-# for code blocks, returns a string of code that is valid.""" # pattern = r"```[\w\s]*\n([\s\S]*?)```" # original
-# regex for code blocks anywhere in the string print('----codeFile', codeFile) pattern = r"^\s*```[ws]*\n([sS]*?)```"
-# codeblocks at start of the string, less eager code_blocks = re.findall(pattern, codeFile, re.MULTILINE) return
-# code_blocks[0] if code_blocks else codeFile
-
 @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 async def generate_code(prompt: str, plan: str, current_file: str,
                         stream_handler: Optional[Callable[Any, Any]] = None) -> str:
@@ -138,8 +101,6 @@ async def generate_code(prompt: str, plan: str, current_file: str,
     completion = openai.ChatCompletion.acreate(
         model=openaimodel,
         temperature=0.7,
-        #   functions=[validCodeFile.openai_schema],
-        #   function_call={ "name": "validCodeFile" },
         messages=[
             {
                 "role": "system",
@@ -196,21 +157,14 @@ async def generate_code(prompt: str, plan: str, current_file: str,
             try:
                 stream_handler(chunk_message['content'].encode('utf-8'))
             except Exception as err:
-                # print('streamHandler error:', err)
-                # print(chunk_message)
                 pass
         collected_messages.append(chunk_message)  # save the message
         if chunk_count < 5:
             chunk_time = time.time() - start_time  # calculate the time delay of the chunk
-            # print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")  #
             chunk_count += 1
 
-    # print(f"Full response received {chunk_time:.2f} seconds after request")
     code_file = "".join([m.get("content", "") for m in collected_messages])
 
-    #   code_file = completion.choices[0].message.content
-    #   print('xxxxxcodeFile', code_file)
-    #   pattern = r"^\s*```[ws]*\n([sS]*?)```" # codeblocks at start of the string, less eager
     pattern = r"```[\w\s]*\n([\s\S]*?)```"  # codeblocks at start of the string, less eager
     code_blocks = re.findall(pattern, code_file, re.MULTILINE)
     return code_blocks[0] if code_blocks else code_file
